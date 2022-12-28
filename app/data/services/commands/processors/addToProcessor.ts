9f17721588556b28b350394ef35b26e5eac26b3d
Benjamin Grosse <bjgrosse@gmail.com>
2022-12-28 20:15:11 +0100

    error fix

--- app/data/services/commands/processors/addToProcessor.ts
+++ app/data/services/commands/processors/addToProcessor.ts
@@ -20,24 +20,33 @@ export default async function processAddTo(
       targetNote = await prisma.note.findFirst({
         select: { id: true, title: true },
         orderBy: { createdAt: "desc" },
+        where: { userId },
       });
       noteDesignator = "last note";
     } else if (remainder.startsWith("last ")) {
       targetNote = await prisma.note.findFirst({
         select: { id: true, title: true },
         orderBy: { createdAt: "desc" },
+        where: { userId },
       });
       noteDesignator = "last ";
     } else {
       const [firstWord] = remainder.split(" ");
       const notes = await prisma.note.findMany({
-        where: { title: { startsWith: firstWord } },
+        // where: { title: { startsWith: firstWord } },
+
+        where: { userId },
         select: { id: true, title: true },
         orderBy: { updatedAt: "desc" },
       });
 
+      const TRIMMED_CHARS = /[^a-zA-Z0-9]/g;
       targetNote = notes.find(
-        (note) => note.title && remainder.startsWith(note.title.toLowerCase())
+        (note) =>
+          note.title &&
+          remainder
+            .replace(TRIMMED_CHARS, "")
+            .startsWith(note.title.replace(TRIMMED_CHARS, "").toLowerCase())
       );
 
       noteDesignator = targetNote?.title;
